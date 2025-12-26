25c07db881e9003ccc7277565cf959fc620fe89f
Will Pringle <willkantorpringle+1@gmail.com>
2025-12-26 11:09:29 -0500

    feat: better cli and package installation

--- run.js
+++ run.js
@@ -6,13 +6,7 @@ import fs from 'node:fs';
 import readline from 'node:readline';
 
 let code = `
-# simple math
-(js/console.log (* (+ 2) (+ 1 7)))
-
-# js interop
-(js/console.log "hello world") #after comment
-
-(js/Math.max 100 123 77)
+(print "Welcome to" (last (split \"${process.argv[1]}\" "/")))
 `;
 
 async function main() {
@@ -20,31 +14,48 @@ async function main() {
     const uargs = process.argv.slice(2);
     if (uargs[0] === '-e' || uargs[0] === '--eval')
       code = uargs[1];
-    else if (uargs[0] === '-r' || uargs[0] === '--repl') {
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        terminal: false
-      });
-      rl.setPrompt("> ");
-      rl.prompt();
 
-      for await (const line of rl) {
-        runCode(line);
-        rl.prompt();
-      }
+    else if (uargs[0] === '-r' || uargs[0] === '--repl') {
+      readEvalPrintLoop();
     } else
       code = fs.readFileSync(uargs[0], 'utf8');
   }
   runCode(code);
 }
 
+async function readEvalPrintLoop(options) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true,
+    historySize: 100,
+    removeHistoryDuplicates: true,
+  });
+
+  rl.setPrompt("> ");
+
+  await rl;
+
+  rl.prompt(true);
+
+  for await (const line of rl) {
+    // optional: ignore empty lines
+    if (line.trim() === "") {
+      rl.prompt(true);
+      continue;
+    }
+
+    await runCode(line);
+    rl.prompt(true);
+  }
+}
+
 function runCode(codeString) {
   const tokens = lexer(codeString);
-  console.log(tokens);
+  //console.log(tokens);
 
   const ast = parser(tokens);
-  console.log(ast);
+  //console.log(ast);
 
   const ret = evalAst(ast);
   console.log(ret);
